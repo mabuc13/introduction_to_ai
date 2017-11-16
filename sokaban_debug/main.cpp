dcdd2f52760bbb545c77df4f9c57c899291249fc
Gilsaa <Gilsaa@live.dk>
2017-11-17 00:48:29 +0100

    Updated

--- sokaban_debug/main.cpp
+++ sokaban_debug/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "sokabanloader.h"
+#include "sokobanloader.h"
 #include <string>
 #include <vector>
 #include <iostream>
@@ -10,22 +10,22 @@ using namespace std;
 
 int main()
 {
-    SokabanLoader test;
+    SokobanLoader test;
     while (true)
     {
 
         int temp = test.loaderFSM("exampleMap.txt", 0, 0);
-        cout << temp << endl;
+        //cout << temp << endl; // Print FSM Status
         if (temp == 3)
             break;
     }
     //test.printMap();
 
     //test.initMap(10, 10);
-    Graph* map = new Graph;
+    //Graph* map = new Graph;
 
-    map = test.vectorToGraph();
-    map->printGraph();
+    //map = test.vectorToGraph();
+    //map->printGraph();
     return 0;
 }
 
